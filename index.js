4733c481255a14fa9be0c9bd2dd4b71590b389e3
devdotjohnangelo <dev.johnangelo@gmail.com>
2024-11-10 14:44:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+alert("asdasdas")
+
+import Swal from 'sweetalert2'
+
+
+
+function myFunction() {
+    Swal.fire({
+        title: "Custom width, padding, color, background.",
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      });
+  
+}
+
